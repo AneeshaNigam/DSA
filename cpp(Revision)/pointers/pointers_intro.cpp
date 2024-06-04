8f0354514e9f1eb5dbca0beeb8c6adf90a4cdc45
AneeshaNigam <aneeshanigam@gmail.com>
2024-06-04 21:10:56 +0530

    notes and basics about pointers

---
+++ cpp(Revision)/pointers/pointers_intro.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    int* p = &x; //int *p = &x; can also be written here
+
+    cout<<&x<<endl;
+    cout<<p;
+
+}
---
+++ cpp(Revision)/pointers/revisiting_variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<sizeof(int)<<endl; //returns the bytes of the databyte 
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(char)<<endl;
+
+
+    int x;
+    cout<<&x;
+} 
